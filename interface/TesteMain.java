c6db48e96e5e58360ff9b8941a4c5565914e7683
Matheus <matheussr61@gmail.com>
2018-11-10 18:27:55 -0200

    Remoção de pastas desnecessárias

--- interface/TesteMain.java
+++
@@ -1,7 +0,0 @@
-public class TesteMain {
-    public static void main(String[] args) {
-        Janela principal = new Janela();
-        principal.criarJanela(500,600);
-        principal.tornarVisivel();
-    }
-}

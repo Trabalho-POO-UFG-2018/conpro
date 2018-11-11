242df6489c579a5310ae92cfe00f9360a217343c
Matheus <matheussr61@gmail.com>
2018-11-10 22:56:09 -0200

    Inicio da implantação das interfaces

--- conpro/src/view/ControladorDeJanelas.java
+++ conpro/src/view/ControladorDeJanelas.java
@@ -1,5 +1,15 @@
 package view;
 
-public class ControladorDeJanelas {
+
+import java.io.IOException;
+import javax.swing.UIManager;
+
+public class ControladorDeJanelas{
+    public static void main(String[] args) throws IOException,Exception {
+        
+        
     
+        TelaInicial inicio = TelaInicial.getInstance();
+        inicio.setVisible(true);
+    }
 }
